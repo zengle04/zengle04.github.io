22369e0ca62b8f2007fc02fa0d25077a579c1ece
zengle04 <zengle091@gmail.com>
2023-12-15 16:49:09 -0500

    More testing

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const starsContainer = document.getElementById("stars-container");
+
+    function createStar() {
+        const star = document.createElement("div");
+        star.className = "star";
+        star.style.left = `${Math.random() * 100}vw`;
+        star.style.top = `${Math.random() * 100}vh`;
+        starsContainer.appendChild(star);
+    }
+
+    // Create initial stars
+    for (let i = 0; i < 100; i++) {
+        createStar();
+    }
+});

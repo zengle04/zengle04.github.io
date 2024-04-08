19c3536b36ea137e45467de8ec0f89d371198188
zengle04 <zengle091@gmail.com>
2024-04-08 00:09:53 -0400

    Beta personal website

--- public/src/main.js
+++ public/src/main.js
@@ -1,26 +1,94 @@
 import * as THREE from 'three';
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement ); 
+//import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { MapControls } from 'three/addons/controls/MapControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-camera.position.z = 5;
+let camera, controls, scene, renderer, loader;
+
+init();
+//render(); // remove when using next line for animation loop (requestAnimationFrame)
+animate();
+
+function init() {
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0x111111 );
+    //scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    loader = new GLTFLoader();
+    LoadModel();
+
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, .1, 1000 );
+    camera.position.set( 0, 5, 0);
+
+    // controls
+
+    controls = new MapControls( camera, renderer.domElement );
+
+    //controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+
+    controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+    controls.dampingFactor = 0.05;
+
+    controls.screenSpacePanning = false;
+
+    controls.minDistance = 0;
+    controls.maxDistance = 50;
+
+    controls.maxPolarAngle = Math.PI / 2;
+
+
+    const ambientLight = new THREE.AmbientLight( 0x555555 , 5);
+    scene.add( ambientLight );
+
+    window.addEventListener( 'resize', onWindowResize );
+
+
+
+}
+
+function LoadModel(){
+    loader.load( 'public/models/scene.gltf', function ( gltf ) {
+
+        scene.add( gltf.scene );
+    
+    }, undefined, function ( error ) {
+    
+        console.error( error );
+    
+    } );
+}
+
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
 
 function animate() {
-	requestAnimationFrame( animate );
-    
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
 
-	renderer.render( scene, camera );
+    requestAnimationFrame( animate );
+
+    controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+
+    render();
+
 }
-animate();
+
+function render() {
+
+    renderer.render( scene, camera );
+
+}
+
